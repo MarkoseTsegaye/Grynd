8defb3c4a3f102627fa7905da11cf79a71710d11
MarkoseTsegaye <markbes493@gmail.com>
2026-06-13 17:15:02 -0400

    changed name

--- app.config.ts
+++ app.config.ts
@@ -4,14 +4,14 @@ const IS_PROD = process.env.APP_ENV === 'production';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: IS_PROD ? 'WorkoutLogger' : 'WorkoutLogger (dev)',
-  slug: 'workout-logger',
+  name: 'Grynd',
+  slug: 'grynd',
   version: '1.0.0',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'dark',
   splash: {
-    image: './assets/splash-icon.png',
+    image: './assets/icon.png',
     resizeMode: 'contain',
     backgroundColor: '#0A0A0A',
   },
